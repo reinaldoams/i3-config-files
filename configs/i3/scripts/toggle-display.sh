8dc506fd6b31eb888377fa09beec001e7209dc14
Reinaldo Assis <reinaldo.assis@99x.io>
2026-07-30 13:42:30 -0300

    changing git checkout to git switch to prevent error when checking out to branch with the same name as a folder inside the current folder; adjusting script so when in mode of both displays the shortcut for toggling display will go to main external display; adjusting shortcut for new monitor being used

--- configs/i3/scripts/toggle-display.sh
+++ configs/i3/scripts/toggle-display.sh
@@ -7,7 +7,8 @@ EXTERNAL="HDMI-1"
 mkdir -p "$(dirname "$STATE_FILE")"
 
 mode=$(cat "$STATE_FILE" 2>/dev/null || echo "external")
-if [ "$mode" = "laptop" ]; then
+echo $mode
+if [ "$mode" = "laptop" ] || [ "$mode" = "both" ]; then
   xrandr --output "$EXTERNAL" --primary --mode 2560x1080 --rate 74.99 --output "$LAPTOP" --off
   echo "external" > "$STATE_FILE"
   gammastep -P -O 3000
