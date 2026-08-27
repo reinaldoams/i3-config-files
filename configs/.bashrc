b08333854e54f20a4421c89bd8f458cc3708d428
Reinaldo Assis <reinaldo.assis@99x.io>
2026-08-27 10:34:28 -0300

    migrating nvim to LazyVim starter; replacing init.lua opts with require("config.lazy"); adding lua/config and lua/plugins scaffolding plus lazy-lock.json, lazyvim.json, .neoconf.json, stylua.toml, README and LICENSE; adding $HOME/.local/bin to PATH in .bashrc; moving calendar scripts to ~/.local/bin/show-calendar and show-calendar-range in i3 bindings; adding $mod+Shift+q powermenu and $mod+t kate bindings with kate floating centered at 50x50 ppt; moving dunst origin to top-right and width to (0, 460); adding battery all to i3status with "bat %percentage" and integer_battery_capacity

--- configs/.bashrc
+++ configs/.bashrc
@@ -137,3 +137,4 @@ export NVM_DIR="$HOME/.nvm"
 # bun
 export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -1,8 +1,2 @@
--- disable netrw at the very start
--- --vim.g.loaded_netrw = 1
---vim.g.loaded_netrwPlugin = 1
-
-vim.opt.number = true
-vim.opt.relativenumber = true
-
-vim.opt.clipboard = "unnamedplus"
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
